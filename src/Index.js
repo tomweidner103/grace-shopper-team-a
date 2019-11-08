c4253f70194a570683241d756181ee646c403138
Thomas Weidner <tomweidner103@gmail.com>
2019-11-07 21:32:34 -0800

    whattt

--- src/Index.js
+++ src/Index.js
@@ -3,6 +3,6 @@ import { render } from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
-import store from '../store';
+import store from '../store'
 
 render(<Provider store={store}><HashRouter>< App /></HashRouter></Provider>, document.getElementById('root') );
